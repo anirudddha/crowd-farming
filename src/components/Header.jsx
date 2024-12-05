6cbfd616b85f9a8fae294c768c684a482dcbb448
Aniruddha <aniruddha662003@gmail.com>
2024-12-05 14:11:28 +0530

    fully functional navbaar and sidebar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
       } else {
         setIsLoggedIn(false);
       }
-    }, 3000); // Check every 2 seconds
+    }, 3000); // Check every 3 seconds
 
     // Cleanup the interval when the component is unmounted
     return () => clearInterval(interval);
@@ -34,8 +34,8 @@ const Header = () => {
     <header>
       <h1>5 Acre Farm</h1>
       <div className="menus">
-        <div className="navmemus">
-          <nav className={isMenuOpen ? 'open' : ''}>
+        <div className={`navmemus ${isMenuOpen ? 'open' : ''}`}>
+          <nav>
             <Link to="/">Home</Link>
             <Link to="/farms">Farm Listings</Link>
             <Link to="/create">Create Campaign</Link>
@@ -57,6 +57,33 @@ const Header = () => {
           <span className="bar"></span>
           <span className="bar"></span>
           <span className="bar"></span>
+        </div>
+      </div>
+
+      {/* Sidebar */}
+      <div className={`sidebar ${isMenuOpen ? 'open' : ''}`}>
+        <div className="hamburger" style={{alignItems:"center",margin:"20px"}} onClick={toggleMenu}>
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </div>
+        <hr />
+        <nav>
+          <Link to="/" onClick={toggleMenu}>Home</Link>
+          <Link to="/farms" onClick={toggleMenu} >Farm Listings</Link>
+          <Link to="/create" onClick={toggleMenu} >Create Campaign</Link>
+          <Link to="/dashboard" onClick={toggleMenu} >Dashboard</Link>
+          <Link to="/profile" onClick={toggleMenu} >Profile</Link>
+        </nav>
+        <div className="auth-links-1">
+          {!isLoggedIn ? (
+            <>
+              <Link to="/login" onClick={toggleMenu}>Sign In</Link>
+              <Link to="/signup" onClick={toggleMenu}>Sign Up</Link>
+            </>
+          ) : (
+            <button onClick={handleLogout}>Logout</button>
+          )}
         </div>
       </div>
     </header>
