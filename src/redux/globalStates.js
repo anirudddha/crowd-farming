efb2ca79f5981966630dda2b62363058052ce71f
Aniruddha <aniruddha662003@gmail.com>
2025-07-18 21:52:07 +0530

    backend removed from here

--- src/redux/globalStates.js
+++ src/redux/globalStates.js
@@ -3,7 +3,7 @@ import {createSlice,configureStore } from '@reduxjs/toolkit';
 const endpoint = createSlice({
     name: 'endpoint',
     initialState: {
-        endpoint: 'http://localhost:5000/api',
+        endpoint: 'https://crowd-farming-backend.onrender.com/api',
     },
     reducers: {
 
